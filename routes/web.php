46784f146af31b6524d2fffcccf39349bb0d82a9
Piero <piero.onorati@virgilio.it>
2021-09-30 18:09:50 +0200

    add

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function() {
+    // pagina di atterraggio dopo il login (con il prefisso, l'url è '/admin')
+    Route::get('/', 'HomeController@index')->name('index');
+});
